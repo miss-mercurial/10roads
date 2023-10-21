ae6453d6b03bff635db7faa386a77242cf70365c
miss-mercurial <sigvardt89@gmail.com>
2023-10-21 18:39:58 +0200

    First commit

---
+++ src/menu.php
@@ -0,0 +1,9 @@
+<!-- NAVIGATION SECTION HERE -->
+<div class="menu">
+    <ul>
+        <li><a href="index.html">Index</a></li>
+        <li><a href="home.html">My Home</a></li>
+        <li><a href="hobby.html">My Hobbies</a></li>
+        <li><a href="work.html">My Work</a></li>
+    </ul>
+</div>
